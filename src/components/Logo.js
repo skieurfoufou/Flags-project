e119b5036ba23c70fe63238ee9ac34eb64af2cd1
Mordehai Benichou <skieurfou@gmail.com>
2022-08-31 18:19:16 +0300

    first commit

---
+++ src/components/Logo.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navigation from "./Navigation";
+
+const Logo = () => {
+  return (
+    <div className="logo">
+      <img src="./img/logo192.png" alt="logo"></img>
+      <h1>React World</h1>
+      <img src="./img/logo192.png" alt="logo"></img>
+      <Navigation />
+    </div>
+  );
+};
+
+export default Logo;
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import Countries from "../components/Countries";
+import Logo from "../components/Logo";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Logo />
+      <Countries />
+    </div>
+  );
+};
+
+export default Home;
