9249cde8d26b35f97ffe5ca2032256fb97dfedf6
skieurfoufou <skieurfou@gmail.com>
2022-09-08 09:56:27 +0300

    fix bug css about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,23 @@
 import React from "react";
 import Logo from "../components/Logo";
-import Navigation from "../components/Navigation";
 
 const About = () => {
   return (
     <div>
       <Logo />
-      <Navigation />
-      <h1>About</h1>
-      <br></br>
+      <div className="container">
+        <br></br>
+        Created By <br></br>Benichou Mordehai ,<br></br>053.8226006
+        skieurfou@hotmail.com
+        <p>Fullstack developer,april 2022</p>
+        <br></br>
+        <hr></hr>
+        <p>React application </p>
+        <hr></hr> <br></br>
+        <hr></hr>
+        <p>Data from an API</p>
+        <hr></hr>
+      </div>
     </div>
   );
 };
